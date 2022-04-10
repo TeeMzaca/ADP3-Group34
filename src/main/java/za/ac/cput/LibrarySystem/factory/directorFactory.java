110375df653290dc0c0d0a08c2845bf81d816a58
Tumelo Mzaca <206006330@mycput.ac.za>
2022-04-10 16:12:07 +0200

    11th commit added a Helper class (A group must have only one helper class) to help me with my testing

--- src/main/java/za/ac/cput/LibrarySystem/factory/directorFactory.java
+++ src/main/java/za/ac/cput/LibrarySystem/factory/directorFactory.java
@@ -5,6 +5,7 @@ import za.ac.cput.LibrarySystem.domain.entity.Director;
 public class directorFactory {
     //creating director objects
     public static Director createDirector(String id, String firstName, String lastName){
+
         //firstName and lastName validation
         if(firstName.equals("") || firstName == null)
             return null;
@@ -15,7 +16,11 @@ public class directorFactory {
             return null;
         if(lastName.isEmpty() || lastName == null)
             return null;
-
+        /**
+        //short-circuit evaluation
+        if(Helper.isNullorEmpty(firstName) || Helper.isNullorEmpty(lastName))
+            return null;
+        */
         return new Director.Builder().setId(id)
                 .setFirstName(firstName)
                 .setLastName(lastName)
---
+++ src/main/java/za/ac/cput/LibrarySystem/util/Helper.java
@@ -0,0 +1,17 @@
+package za.ac.cput.LibrarySystem.util;
+
+import java.util.UUID;
+
+public class Helper {
+    //A group must have only one helper class
+    /**
+    public static boolean isNullorEmpty(String s){
+        return (s == null || s.equals("")  || s.isEmpty() || s.equalsIgnoreCase("null"));
+    }
+
+    //unique id number
+    public static String generateld() {
+        return UUID.randomUUID().toString();
+    }
+     */
+}
