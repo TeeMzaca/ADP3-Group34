89f0ef2d12a783d95af995cd9e1e919fb98155eb
Tumelo Mzaca <206006330@mycput.ac.za>
2022-04-10 12:57:54 +0200

    9th commit run the test for filmFactoryTest

--- src/test/java/za/ac/cput/LibrarySystem/factory/filmFactoryTest.java
+++ src/test/java/za/ac/cput/LibrarySystem/factory/filmFactoryTest.java
@@ -1,7 +1,16 @@
 package za.ac.cput.LibrarySystem.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.LibrarySystem.domain.entity.Film;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class filmFactoryTest {
+
+    @Test
+    public void test() {
+        Film film = filmFactory.createFilm("1", "How To Pass ADP3", "10 May 2022");
+        assertNotNull(film);
+    }
 
 }
