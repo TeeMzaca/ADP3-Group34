201579376cc41b6f09faddc0a3f2cdeedc7cd5b5
Tumelo Mzaca <206006330@mycput.ac.za>
2022-04-10 16:32:44 +0200

    12th commit added a directorRepository

--- src/main/java/za/ac/cput/LibrarySystem/util/Helper.java
+++ src/main/java/za/ac/cput/LibrarySystem/util/Helper.java
@@ -5,12 +5,13 @@ import java.util.UUID;
 public class Helper {
     //A group must have only one helper class
     /**
-    public static boolean isNullorEmpty(String s){
+     //To test if null or empty
+    public static boolean isNuLLorEmpty(String s){
         return (s == null || s.equals("")  || s.isEmpty() || s.equalsIgnoreCase("null"));
     }
 
     //unique id number
-    public static String generateld() {
+    public static String generateID() {
         return UUID.randomUUID().toString();
     }
      */
