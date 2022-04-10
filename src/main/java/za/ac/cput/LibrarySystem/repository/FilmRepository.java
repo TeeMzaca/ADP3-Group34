00e4cf2ebb3739ba737e12374699a0a521328b52
Tumelo Mzaca <206006330@mycput.ac.za>
2022-04-10 19:25:36 +0200

    17th commit

--- src/main/java/za/ac/cput/LibrarySystem/repository/FilmRepository.java
+++ src/main/java/za/ac/cput/LibrarySystem/repository/FilmRepository.java
@@ -1,4 +1,33 @@
 package za.ac.cput.LibrarySystem.repository;
 
-public class FilmRepository {
+import za.ac.cput.LibrarySystem.domain.entity.Film;
+
+import java.util.Set;
+
+public class FilmRepository implements IFilmRepository{
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public Set<Film> getAll() {
+        return null;
+    }
 }
--- src/main/java/za/ac/cput/LibrarySystem/repository/IFilmRepository.java
+++ src/main/java/za/ac/cput/LibrarySystem/repository/IFilmRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.LibrarySystem.repository;
 
-public interface IFilmRepository {
+import za.ac.cput.LibrarySystem.domain.entity.Film;
+
+import java.util.Set;
+
+public interface IFilmRepository extends IRepository<Film, String>{
+    //read, update, create and delete
+    public Set<Film> getAll();
 }
